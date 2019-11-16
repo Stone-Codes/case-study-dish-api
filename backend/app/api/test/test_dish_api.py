3f708f105c600ea9358862448d4243e89afdfbf6
TestForCodes <mail@dietz.codes>
2019-11-16 13:27:54 +0100

    added test for dish model added test for searching for dishes fixed url query parameter in sorting test

--- backend/app/api/test/test_dish_api.py
+++ backend/app/api/test/test_dish_api.py
@@ -14,6 +14,15 @@ def sample_dishes():
     Dish.objects.create(name="Spaghetti", price=5.99)
 
 
+class DishModelTests(TestCase):
+    """Test for the dish model"""
+
+    def test_dish_str(self):
+        """Test the dish str method"""
+        dish = Dish.objects.create(name="Gemüsesuppe", price=4.55)
+
+        self.assertEqual(str(dish), dish.name)
+
 class DishApiTests(TestCase):
     """Test the dish api"""
 
@@ -36,7 +45,7 @@ class DishApiTests(TestCase):
         """Test for filtering dishes by price descending"""
         sample_dishes()
 
-        res = self.client.get(DISH_URL, {'sort': '-price'})
+        res = self.client.get(DISH_URL, {'ordering': '-price'})
 
         dishes = Dish.objects.all().order_by('-price')
         serializer = DishSerializer(dishes, many=True)
@@ -49,7 +58,7 @@ class DishApiTests(TestCase):
         """Test for filtering dishes by name descending"""
         sample_dishes()
 
-        res = self.client.get(DISH_URL, {'sort': '-name'})
+        res = self.client.get(DISH_URL, {'ordering': '-name'})
 
         dishes = Dish.objects.all().order_by('-name')
         serializer = DishSerializer(dishes, many=True)
@@ -61,7 +70,7 @@ class DishApiTests(TestCase):
         """Test for filtering dishes by price ascending"""
         sample_dishes()
 
-        res = self.client.get(DISH_URL, {'sort': 'price'})
+        res = self.client.get(DISH_URL, {'ordering': 'price'})
 
         dishes = Dish.objects.all().order_by('price')
         serializer = DishSerializer(dishes, many=True)
@@ -73,10 +82,47 @@ class DishApiTests(TestCase):
         """Test for filtering dishes by name ascending"""
         sample_dishes()
 
-        res = self.client.get(DISH_URL, {'sort': 'name'})
+        res = self.client.get(DISH_URL, {'ordering': 'name'})
 
         dishes = Dish.objects.all().order_by('name')
         serializer = DishSerializer(dishes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_searching_for_dish_name(self):
+        """Test for searching for a dish name"""
+        sample_dishes()
+
+        res = self.client.get(DISH_URL, {'search': 'Schnitzel'})
+
+        dish = Dish.objects.filter(name='Schnitzel')
+        serializer = DishSerializer(dish, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_searching_partial_dish_names(self):
+        """Test for searching a partial dish name"""
+        sample_dishes()
+
+        res = self.client.get(DISH_URL, {'search': 'ghetti'})
+
+        dish = Dish.objects.filter(name='Spaghetti')
+        serializer = DishSerializer(dish, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+
+    def test_ignoring_upper_lower_case_in_search(self):
+        """Test that uppercase or lowercase are ignored in a search"""
+        sample_dishes()
+
+        res = self.client.get(DISH_URL, {'search': 'sCHniTzEl'})
+
+        dish = Dish.objects.filter(name='Schnitzel')
+        serializer = DishSerializer(dish, many=True)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
