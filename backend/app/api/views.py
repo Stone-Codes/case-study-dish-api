86013477652174c4a66a708caf761796d25cf1a8
TestForCodes <mail@dietz.codes>
2019-11-16 13:32:25 +0100

    added searching and ordering to dishes viewset

--- backend/app/api/views.py
+++ backend/app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
+from rest_framework import filters
 
 from api.models import Dish
 from api.serializers import DishSerializer
@@ -9,6 +10,9 @@ class DishViewSet(GenericViewSet, ListModelMixin):
 
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['name', 'price']
+    search_fields = ['name']
 
     def get_queryset(self):
         return self.queryset
